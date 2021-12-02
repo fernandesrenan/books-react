ef62f4b48637defe27abd46fbbbcc4ee6be4f8f5
Renan Fernandes <renan.fernandes@cwi.com.br>
2021-12-02 10:02:04 -0300

    add login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppDispatch } from "../../hooks";
+import { signin } from "../../userSlice";
 
-export function Login() {
-  return <div>Login</div>;
-}
+export const Login: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const [login, setLogin] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const handleClick = () => {
+    if (login && password) {
+      dispatch(signin({ userName: login, isAuthenticated: true }));
+    }
+  };
+
+  return (
+    <div>
+      Login
+      <input type="text" onChange={(e) => setLogin(e.target.value)} />
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
+};
